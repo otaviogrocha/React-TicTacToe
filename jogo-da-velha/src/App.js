cffedc1cfc12b927a8cb93ec27aef9af381180a6
Otávio Rocha <otavio.rocha@expertcode.com.br>
2024-04-26 16:07:53 -0300

    first commit

---
+++ jogo-da-velha/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href='#' className='action'>Sobre</a>
+    </div>
+  );
+}
+
+export default App;
